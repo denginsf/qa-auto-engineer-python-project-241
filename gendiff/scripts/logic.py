62f497b3852145363de6e1ea6b6bcdb50b5c4acb
Aleksei Dengin <wrk.dengin@gmail.com>
2026-03-04 00:51:17 +0300

    Add file parsing

--- gendiff/scripts/logic.py
+++ gendiff/scripts/logic.py
@@ -7,5 +7,7 @@ def get_args():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
-    return args
+    parsed_file1 = json.load(open(f'qa-auto-engineer-python-project-241/gendiff/{args.first_file}'))
+    parsed_file2 = json.load(open(f'qa-auto-engineer-python-project-241/gendiff/{args.second_file}'))
+    print(parsed_file1)
+    return parsed_file1, parsed_file2
