eb9bff6e06ad64c3cf81772a78c4e145ffc041bc
Aleksei Dengin <wrk.dengin@gmail.com>
2026-03-04 00:15:30 +0300

    Fix project structure, add args parsing

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,7 @@
-import argparse
-
+from gendiff.scripts.logic import get_args
 
 def main():
-    parser = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.')
-
-
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.print_help()
+    get_args()
 
 if __name__ == "__main__":
     main()
