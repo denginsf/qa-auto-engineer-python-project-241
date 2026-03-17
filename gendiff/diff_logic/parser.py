08a17e67c6cecdae0b39495f7a0146f549a4f013
Aleksei Dengin <wrk.dengin@gmail.com>
2026-03-17 18:54:16 +0300

    Fix parser logic

--- gendiff/diff_logic/parser.py
+++ gendiff/diff_logic/parser.py
@@ -3,19 +3,18 @@ import json
 import yaml
 
 
+def parse_file(file_name):
+    with open(file_name) as f:
+        if file_name.lower().endswith('.json'):
+            parsed_file = json.load(f)
+        elif file_name.lower().endswith(('.yml', '.yaml')):
+            parsed_file = yaml.safe_load(f)
+    return parsed_file
+
+
 def parse_files(file_name1, file_name2):
-    with (
-        open(file_name1) as f1, 
-        open(file_name2) as f2
-        ):
-        if file_name1.lower().endswith('.json') \
-        and file_name2.lower().endswith('.json'):
-            parsed_file1 = json.load(f1)
-            parsed_file2 = json.load(f2)
-        elif file_name1.lower().endswith(('.yml', '.yaml')) \
-        and file_name2.lower().endswith(('.yml', '.yaml')):
-            parsed_file1 = yaml.safe_load(f1)
-            parsed_file2 = yaml.safe_load(f2)
+    parsed_file1 = parse_file(file_name1)
+    parsed_file2 = parse_file(file_name2)
     return parsed_file1, parsed_file2
 
 
