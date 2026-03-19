d8137cae1f3c3bfe9aad6e149dc08349d1c7ad02
Aleksei Dengin <wrk.dengin@gmail.com>
2026-03-19 22:19:00 +0300

    Fix linter for tests

--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -1,17 +1,19 @@
-from gendiff import generate_diff
 from pathlib import Path
-import json
+
+from gendiff import generate_diff
 
 
 def get_test_data_path(filename):
     return Path(__file__).parent / "test_data" / filename
 
+
 def read_file(filename):
     return get_test_data_path(filename).read_text()
 
-## Stylish
+# Stylish
 
 # JSON тесты
+
 
 def test_stylish_formatter_plain_json_full_diff():
     file1 = str(get_test_data_path('plain_json_first.json'))
@@ -20,6 +22,7 @@ def test_stylish_formatter_plain_json_full_diff():
     actual = generate_diff(file1, file2)
     assert expected == actual
 
+
 def test_stylish_formatter_plain_json_empty_first_file():
     file1 = str(get_test_data_path('empty_json.json'))
     file2 = str(get_test_data_path('plain_json_second.json'))
@@ -27,12 +30,14 @@ def test_stylish_formatter_plain_json_empty_first_file():
     actual = generate_diff(file1, file2)
     assert actual == expected
 
+
 def test_stylish_formatter_plain_json_empty_second_file():
     file1 = str(get_test_data_path('plain_json_first.json'))
     file2 = str(get_test_data_path('empty_json.json'))
     expected = read_file("empty_second_expected.txt")
     actual = generate_diff(file1, file2)
     assert expected == actual
+
 
 def test_stylish_formatter_plain_json_empty_files():
     file1 = str(get_test_data_path('empty_json.json'))
@@ -44,12 +49,14 @@ def test_stylish_formatter_plain_json_empty_files():
 # YAML тесты
 # Еще проверяею расширение .yml .yaml
 
+
 def test_stylish_formatter_full_diff_plain_yaml():
     file1 = str(get_test_data_path('test_file1.yml'))
     file2 = str(get_test_data_path('test_file2.yaml'))
     expected = read_file("plain_json_and_yaml_gendiff_result.txt")
     actual = generate_diff(file1, file2)
     assert actual == expected
+
 
 def test_stylish_formatter_empty_first_file_plain_yaml():
     file1 = str(get_test_data_path('empty_yaml.yml'))
@@ -58,12 +65,14 @@ def test_stylish_formatter_empty_first_file_plain_yaml():
     actual = generate_diff(file1, file2)
     assert actual == expected
 
+
 def test_stylish_formatter_empty_second_file_plain_yaml():
     file1 = str(get_test_data_path('test_file1.yml'))
     file2 = str(get_test_data_path('empty_yaml.yml'))
     expected = read_file("empty_second_expected.txt")
     actual = generate_diff(file1, file2)
     assert actual == expected
+
 
 def test_stylish_formatter_empty_files_plain_yaml():
     file1 = str(get_test_data_path('empty_yaml.yml'))
@@ -73,7 +82,7 @@ def test_stylish_formatter_empty_files_plain_yaml():
     assert actual == expected
 
 
-## Plain
+# Plain
 
 # JSON тесты plain formatter
 
@@ -84,6 +93,7 @@ def test_plain_formatter_plain_json_full_diff():
     actual = generate_diff(file1, file2, 'plain')
     assert expected == actual
 
+
 def test_plain_formatter_plain_json_empty_first_file():
     file1 = str(get_test_data_path('empty_json.json'))
     file2 = str(get_test_data_path('plain_json_second.json'))
@@ -91,12 +101,14 @@ def test_plain_formatter_plain_json_empty_first_file():
     actual = generate_diff(file1, file2, 'plain')
     assert actual == expected
 
+
 def test_plain_formatter_plain_json_second_file():
     file1 = str(get_test_data_path('plain_json_first.json'))
     file2 = str(get_test_data_path('empty_json.json'))
     expected = read_file("plain_formatter_empty_second_expected.txt")
     actual = generate_diff(file1, file2, 'plain')
     assert expected == actual
+
 
 def test_plain_formatter_plain_json_files():
     file1 = str(get_test_data_path('empty_json.json'))
@@ -108,12 +120,14 @@ def test_plain_formatter_plain_json_files():
 # YAML тесты plain formatter
 # Еще проверяею расширение .yml .yaml
 
+
 def test_plain_formatter_full_diff_plain_yaml():
     file1 = str(get_test_data_path('test_file1.yml'))
     file2 = str(get_test_data_path('test_file2.yaml'))
     expected = read_file("plain_formatter_json_and_yaml_gendiff_result.txt")
     actual = generate_diff(file1, file2, 'plain')
     assert actual == expected
+
 
 def test__plain_formatter_empty_first_plain_yaml():
     file1 = str(get_test_data_path('empty_yaml.yml'))
@@ -122,12 +136,14 @@ def test__plain_formatter_empty_first_plain_yaml():
     actual = generate_diff(file1, file2, 'plain')
     assert actual == expected
 
+
 def test__plain_formatter_empty_second_plain_yaml():
     file1 = str(get_test_data_path('test_file1.yml'))
     file2 = str(get_test_data_path('empty_yaml.yml'))
     expected = read_file("plain_formatter_empty_second_expected.txt")
     actual = generate_diff(file1, file2, 'plain')
     assert actual == expected
+
 
 def test__plain_formatter_empty_files_plain_yaml():
     file1 = str(get_test_data_path('empty_yaml.yml'))
